06fa49decd36c17913faf1883f159d14133be5d9
nboehlje <nathanialboehlje255@gmail.com>
2025-12-11 23:23:17 -0600

    Completed day2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,16 +1,16 @@
 use std::fs;
 
 pub fn run() {
-    let file_path = "data/day2_test_puzzle_input.txt";
+    let file_path = "data/day2_puzzle_input.txt";
 
     println!("================= [BEGIN DAY 2] =================");
     println!("(PART 1):\n");
     let part1_ans = get_part1_answer(file_path);
-    print!("ANS: [{part1_ans}]");
+    println!("ANS: [{part1_ans}]");
 
     println!("\n(PART 2):\n");
     let part2_ans = get_part2_answer(file_path);
-    print!("ANS: [{part2_ans}]");
+    println!("ANS: [{part2_ans}]");
 }
 
 fn get_part1_answer(file_name: &str) -> u64 {
@@ -49,6 +49,7 @@ fn get_part2_answer(file_name: &str) -> u64 {
             .trim()
             .parse()
             .expect("Invalid string, should be a number");
+
         running_total += sum_invalid_ids_within_range_part2(lower, upper);
     }
 
@@ -79,23 +80,25 @@ fn sum_invalid_ids_within_range_part2(lower: u64, upper: u64) -> u64 {
     let mut running_total: u64 = 0;
     for id in lower..=upper {
         let id_as_str = id.to_string();
-        let str_len = id_as_str.chars().count();
-        print!("{id_as_str} -> ");
-        for i in 0..str_len {
-            let test = &id_as_str[i..i+1];
-            print!(" |{test}");
+        let str_len = id_as_str.len();
+
+        for n in 1..=(str_len / 2) {
+            if str_len % n != 0 {
+                continue;
+            }
+
+            let mut iter = id_as_str.as_bytes().chunks_exact(n);
+            let first_chunk = match iter.next() {
+                Some(chunk) => chunk, 
+                None => { continue; } 
+            };
+
+            if iter.all(|chunk| chunk == first_chunk) {
+                running_total += id;
+                break;
+            }
         }
-        println!();
-
-
     }
 
     running_total
-}
-
-struct FiniteStateMachine {
-
-}
-fn finite_state_machine() {
-    //
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::env;
+
 mod day1;
 mod day2;
 
 fn main() {
-    // day1::run();
-    day2::run();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        let user_choice = &args[1];
+        match user_choice.as_str() {
+            "day1" => day1::run(),
+            "day2" => day2::run(),
+            _ => panic!("Invalid argument"),
+        }
+    } else {
+        println!("No argument given, exiting...");
+    }
 }
